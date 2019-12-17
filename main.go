54ab1fdf0f36928b8b6583e7acfcbf66ccb19a71
Yanzhi Li <muslem@msn.com>
2019-12-17 18:35:30 +0800

    Update main.go

--- main.go
+++ main.go
@@ -8,6 +8,7 @@ import (
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/client-go/kubernetes"
 	"k8s.io/client-go/rest"
+	"k8s.io/client-go/plugin/pkg/client/auth"
 	//
 	// Uncomment to load all auth plugins
 	// _ "k8s.io/client-go/plugin/pkg/client/auth"
